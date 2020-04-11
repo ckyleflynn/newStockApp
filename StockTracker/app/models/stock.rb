582c7a793ecb1a6480271494a443fcb767f173c8
ckyleflynn <ckflynn@csustudent.net>
2020-04-11 01:07:05 -0400

    stock lookup

---
+++ StockTracker/app/models/stock.rb
@@ -0,0 +1,17 @@
+class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence:true
+
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: 'Tpk_ef1430b56ffc424ab5af1d379d31cf6b',
+                                  endpoint: 'https://sandbox.iexapis.com/v1')
+
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+    rescue => exception
+      return nil
+  end
+  end
+end
---
+++ StockTracker/config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :user_stocks
+  devise_for :users
+  root 'home#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  get 'search_stock', to: 'stocks#search'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
